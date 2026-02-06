7d75a75675df22bc909954762540d960e61579d5
NandaCorreaa <fernandacorrea1909@gmail.com>
2026-02-06 10:28:59 -0300

    Estilizando a Página Inicial e Finalizando a Página de Perfil

--- src/components/Inicio/Inicio.jsx
+++ src/components/Inicio/Inicio.jsx
@@ -1,8 +1,9 @@
 import ImgInicial from '../../assets/imagemInicial.png'
+import styles from './Inicio.module.scss'
 
-function Inicio(){
-    return(
-        <section>
+function Inicio() {
+    return (
+        <section className={styles.containerTelaInicial}>
             <div>
                 <h1>Projetos Sociais que transformam</h1>
                 <p>Conectamos sua empresa com projetos sociais impactantes. Juntos, criamos mudanças reais na comunidade.</p>
