5966c380260040a651cf16632261ac9fb5b166bf
NandaCorreaa <fernandacorrea1909@gmail.com>
2026-02-11 09:56:31 -0300

    Desenvolvendo a Footer e finalizando o projeto desktop

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,24 @@
+import Logo from '../../assets/logo.png'
+import Whatsapp from '../../assets/whatsapp.png'
+import Instagram from '../../assets/instagram.png'
+import Linkedin from '../../assets/linkedin.png'
+
+import styles from './Footer.module.scss'
+
 function Footer() {
     return (
-        <>
-            <h2>Eu sou a Footer</h2>
-        </>
+        <footer>
+            <div>
+                <img src={Logo} alt="Logo da Footer" />
+                <h2>Connect</h2>
+            </div>
+            <p>© 2026 Vai na Web & Empower. Todos os direitos reservados. Projeto destinado exclusivamente a fins educativos.</p>
+            <nav className={styles.navFooter}>
+                <img src={Whatsapp} alt="Logo do Whatsapp" />
+                <img src={Instagram} alt="Logo do Instagram" />
+                <img src={Linkedin} alt="Logo do Linkedin" />
+            </nav>
+        </footer>
     )
 }
 
